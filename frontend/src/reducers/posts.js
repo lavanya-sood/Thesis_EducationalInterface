4c8ffc30bcef53fec03dbec435bc95bd5c92776e
Lavanya Sood <lavanyasood09@gmail.com>
2021-07-03 22:07:34 +1000

    Added redux

---
+++ frontend/src/reducers/posts.js
@@ -0,0 +1,10 @@
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return state;
+        case 'CREATE':
+            return state;
+        default:
+            break;
+    }
+};
