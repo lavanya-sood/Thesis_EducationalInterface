1b28f26e140fb0b4dd8fd7a350d45af9f862faa7
Lavanya Sood <lavanyasood09@gmail.com>
2021-07-04 15:54:05 +1000

    Added appbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { AppBar, Typography, Button, Toolbar} from '@material-ui/core';
 
 const App = () => {
     return (
         <div>
-            <h1> App </h1>
+            <AppBar position='sticky' color='primary'>
+                <Toolbar>
+                    <Typography variant="h6"> Introduction to HTML</Typography>
+                    <Button color="inherit">Welcome, user</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 };
