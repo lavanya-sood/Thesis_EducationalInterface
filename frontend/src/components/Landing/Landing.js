4f4bd676c83d6b54c77b999e4120cc71b9104f9c
Lavanya Sood <lavanyasood09@gmail.com>
2021-07-05 16:14:59 +1000

    Finished Landing page

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import HeaderBar from '../HeaderBar/HeaderBar.js';
-import { AppBar, Typography, Button, Toolbar, Container, TextField} from '@material-ui/core';
+import { AppBar,Paper ,Typography, Button, Toolbar,Grid, Container, TextField} from '@material-ui/core';
 import useStyles from './styles.js';
 
 const Landing = () => {
     const classes = useStyles();
     return (
-        <Container maxWidth='lg' className={classes.outsideContainer}>
-            <Typography variant="h1"> Introduction to Hypertext Markup Language</Typography>
-            <Container maxWidth='md'>
-                <Typography variant="h4"> Enter Login Code </Typography>
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                <Button variant="contained" color="primary"> Start Module </Button>
-            </Container>
-        </Container>
+        <Grid container spacing={0} direction="column" alignItems="center" justify="center" className={classes.outsideContainer}>
+            <Typography className={classes.heading} variant="h2" align="center"> Introduction to Hypertext Markup Language</Typography>
+            <Paper className={classes.insideContainer}>
+                <Typography variant="h4" align="center"> Enter Login Code </Typography>
+                <TextField id="outlined-basic" label="Enter Code" variant="outlined" className={classes.textBoxCode} />
+                <Button variant="contained" color="primary" className={classes.startButton}> Start Module </Button>
+            </Paper>   
+        </Grid> 
     );
 };
 
--- frontend/src/components/Landing/styles.js
+++ frontend/src/components/Landing/styles.js
@@ -3,6 +3,32 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
     outsideContainer: {
-        backgroundColor: pink,
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center',
+        // backgroundColor: '#bfe9ff',
+        backgroundImage: 'linear-gradient(#bfe9ff, #94daff)',
+    },
+    insideContainer: { 
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        padding: '10px',
+        height: '40vh',
+        width: '40vw',
+    },
+    startButton: {
+        paddingTop: '15px',
+        paddingBottom: '15px',
+        paddingLeft: '30px',
+        paddingRight: '30px',
+    },
+    textBoxCode: {
+        width: '25vw',
+    },
+    heading: {
+        paddingBottom: '30px',
     }
 }));
