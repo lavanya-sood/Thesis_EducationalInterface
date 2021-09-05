54d52019134b48faec8b950878dc89bb39a05bc3
Lavanya Sood <lavanyasood09@gmail.com>
2021-09-05 21:35:22 +1000

    Edited variables

--- backend/index.js
+++ backend/index.js
@@ -29,11 +29,11 @@ app.get('/',(req,res) => {
 
 // setup the url
 //const CONNECTION_URL =  'mongodb+srv://thesisoriginal:thesispassword123@cluster0.qgqkr.mongodb.net/Module?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
 // connect the application to the mongoDB database
 mongoose.connect(process.env.CONNECTION_URL ,{useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=> app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`)))
+    .then(()=> app.listen(PORT, ()=>console.log(`Server running on port ${process.env.PORT || 5000}`)))
     .catch((error)=> error.message);
 
 mongoose.set('useFindAndModify',false);
