bac590adc6357c16c7fc54bbf6bd29ed53c41ac1
Lavanya Sood <lavanyasood09@gmail.com>
2021-09-05 20:26:43 +1000

    Added environment variables

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,13 @@ import cors from 'cors';
 import moduleRoutes from './routes/module.js';
 import answerRoutes from './routes/answers.js';
 import navRoutes from './routes/pageNav.js';
+import dotenv from 'dotenv';
+
+
 
 // create the express application
 const app = express();
+dotenv.config();
 
 // allow cors
 app.use(bodyParser.json({limit:"30mb", extended: true}));
@@ -20,11 +24,11 @@ app.use('/answers',answerRoutes);
 app.use('/navigation',navRoutes);
 
 // setup the url
-const CONNECTION_URL =  'mongodb+srv://thesisoriginal:thesispassword123@cluster0.qgqkr.mongodb.net/Module?retryWrites=true&w=majority';
+//const CONNECTION_URL =  'mongodb+srv://thesisoriginal:thesispassword123@cluster0.qgqkr.mongodb.net/Module?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 // connect the application to the mongoDB database
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL ,{useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`)))
     .catch((error)=> error.message);
 
